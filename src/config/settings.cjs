526938b571b015299515cb322e233c8d3bf99dae
gayaneditz-bit <gayaneditz@gmail.com>
2026-06-26 10:35:00 +0530

    Create settings.cjs

---
+++ src/config/settings.cjs
@@ -0,0 +1,38 @@
+module.exports = {
+  "PREFIX": ".",
+  "OWNERS": [
+    "94769421870@s.whatsapp.net"
+  ],
+  "PORT": "3000",
+  "AUTOREADSTATUS": true,
+  "MODE": "public",
+  "BOTNUMBER": "94769421870",
+  "OMDBAPI_KEY": "76cb7f39",
+  "READCMD": true,
+  "AUTOVOICE": false,
+  "AUTOSTICKER": true,
+  "AUTOREPLY": true,
+  "AUTOREACT": false,
+  "WELCOME": false,
+  "ANTIBAD": true,
+  "ANTIBOT": true,
+  "ANTILINK": false,
+  "ANTICALL": false,
+  "ANTIDELETE": true,
+  "ALLWAYSONLINE": true,
+  "MOROCCOBLOCK": true,
+  "OWNERNAME": "dtz-naruto",
+  "NEWSJID": "120363393420637374@g.us",
+  "MOVIEJID": false,
+  "ANIMEJID": "120363367650594508@g.us",
+  "AUTOTYPING": false,
+  "AUTORECORDING": true,
+  "AUTONEWS": false,
+  "OWNERREACT": false,
+  "EMOJI": "🏷",
+  "AIMODE": false,
+  "BANNED": [],
+  "SUDO": [
+    "94769421870"
+  ]
+};
